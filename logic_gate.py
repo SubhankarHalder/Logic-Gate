74ba4171ac334143643f4cccf1b0dd15936ddb12
Subhankar Halder <subhankarvirgoan@gmail.com>
2019-12-05 07:46:53 +0530

    Second Commit

--- logic_gate.py
+++ logic_gate.py
@@ -1,1 +1,42 @@
 # This is a program that implements Logic Gates
+
+class LogicGate:
+    """This is a class on Logic Gate"""
+    def __init__(self,n):
+        self.label = n
+        self.output = None
+
+    def getLabel(self):
+        return self.label
+
+    def getOutput(self):
+        self.output = self.performGateLogic()
+        return self.output
+
+class BinaryGate(LogicGate):
+
+    def __init__(self,n):
+        super(BinaryGate,self).__init__(n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def getPinA(self):
+        return int(input("Enter Pin A input for gate "+ self.getLabel()+"--->"))
+
+    def getPinB(self):
+        return int(input("Enter Pin B input for gate "+ self.getLabel()+"---->"))
+
+class UnaryGate(LogicGate):
+
+    def __init__(self, n):
+        super(UnaryGate,self).__init__(n)
+
+        self.pin = None
+
+    def getPin(self):
+        return int(input("Enter Pin input for gate "+ self.getLabel()+"---->"))
+
+    
+
+
